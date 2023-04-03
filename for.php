75e98d10f1fcc7199919ed1e9a35107eec573dc6
MARIA VITORIA MAGALHAES HOBOLD <hobold.7994@aluno.pr.senac.br>
2023-04-03 08:06:41 -0300

    laço for

---
+++ for.php
@@ -0,0 +1,44 @@
+
+<?php
+    echo "1";
+    echo "<br>";
+    echo "2";
+    echo "<br>";
+    echo "3";
+    echo "<br>";
+    echo "4";
+    echo "<br>";
+    echo "5";
+    echo "<br>";
+    echo "6";
+    echo "<br>";
+    echo "7";
+    echo "<br>";
+    echo "8";
+    echo "<br>";
+    echo "9";
+    echo "<br>";
+    echo "10";
+    echo "<br>";
+
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+    for($numero=0; $numero<=10; $numero++){
+        echo $numero . "<br>";
+    }
+
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+    for ($numero= 0; $numero<=100; $numero++){
+		if ($numero % 2 == 1){
+            echo $numero . "<br>";
+		}
+    }
+?>
